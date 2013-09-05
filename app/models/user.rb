56521c7fad59f28b9a7e044fecd1ae67ec2729ca
Jayan et Clement <jfk303@nyu.edu clementpigeon@gmail.com>
2013-09-05 17:59:27 -0400

    MAGIC HAPPENS.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :user_name
+  #attr_reader :id
 
   validates :user_name, presence: true, uniqueness: true
 
@@ -17,16 +18,6 @@ class User < ActiveRecord::Base
   primary_key: :id
   )
 
-  # todo: add condition checking if this holds for ALL the subordinate questions
-  has_many :answer_choices, through: :responses, source: :answer_choice
-  has_many :questions, through: :answer_choices, source: :question
-  has_many :fully_completed_polls, through: :questions, source: :poll, uniq: true,
-    conditions: "
-
-    "
-
-
-
     #polls where we've answered at least one question
     #want: polls where all their questions
     #...have at least one answer choice
@@ -35,7 +26,104 @@ class User < ActiveRecord::Base
 
   has_many :answer_choices, through: :responses, source: :answer_choice
   has_many :questions, through: :answer_choices, source: :question
-  has_many :completed_polls, through: :questions, source: :poll, uniq: true
+  has_many :completed_polls, through: :questions, source: :poll, uniq: true,
+  conditions: proc {"(SELECT
+        COUNT(*) AS num_questions
+        FROM
+        polls AS p
+        JOIN
+        questions
+        ON
+        p.id = questions.poll_id
+        WHERE
+        p.id = polls.id) = (
+        SELECT
+        COUNT(*) AS num_questions
+        FROM
+        polls AS p
+        JOIN
+        questions
+        ON
+        p.id = questions.poll_id
+        JOIN
+        answer_choices
+        ON
+        answer_choices.question_id = questions.id
+        JOIN
+        responses
+        ON
+        responses.answer_choice_id = answer_choices.id
+        WHERE
+        p.id = polls.id AND responses.respondent_id = '#{id}')"}
+
+        has_many :incomplete_polls, through: :questions, source: :poll, uniq: true,
+        conditions: proc {"(SELECT
+              COUNT(*) AS num_questions
+              FROM
+              polls AS p
+              JOIN
+              questions
+              ON
+              p.id = questions.poll_id
+              WHERE
+              p.id = polls.id) <> (
+              SELECT
+              COUNT(*) AS num_questions
+              FROM
+              polls AS p
+              JOIN
+              questions
+              ON
+              p.id = questions.poll_id
+              JOIN
+              answer_choices
+              ON
+              answer_choices.question_id = questions.id
+              JOIN
+              responses
+              ON
+              responses.answer_choice_id = answer_choices.id
+              WHERE
+              p.id = polls.id AND responses.respondent_id = '#{id}')"}
+
+  def num_questions_for_poll
+    Poll.find_by_sql(<<-SQL)
+      SELECT
+        COUNT(*) AS num_questions
+        FROM
+        polls
+        JOIN
+        questions
+        ON
+        polls.id = questions.poll_id
+        WHERE
+        polls.id = poll.id
+    SQL
+  end
+
+  def num_answered_questions
+    Poll.find_by_sql(<<-SQL)
+      SELECT
+        COUNT(*) AS num_questions
+        FROM
+        polls
+        JOIN
+        questions
+        ON
+        polls.id = questions.poll_id
+        JOIN
+        answer_choices
+        ON
+        answer_choices.question_id = questions.id
+        JOIN
+        responses
+        ON
+        responses.answer_choice_id = answer_choices.id
+        WHERE
+        polls.id = poll.id AND responses.respondent_id = #{self.id}
+    SQL
+
+  end
 
   def sql_test
     Poll.find_by_sql(<<-SQL)
